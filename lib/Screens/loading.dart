72bc96c9f466f78a8077b6c3678340826a0619ef
chan <hayanmelon@gmail.com>
2022-07-20 06:29:16 +0900

    15 10:57

--- lib/Screens/loading.dart
+++ lib/Screens/loading.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/data/network.dart';
 
-import 'package:http/http.dart' as http;
 import 'package:weatherapp/data/my_location.dart';
-import 'dart:convert';
+
 import 'package:weatherapp/data/my_location.dart';
 
 class Loading extends StatefulWidget {
@@ -12,43 +12,46 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  double? latitude2;
-  double? longitude2;
-
+  double? latitude3;
+  double? longitude3;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getLocation();
-    fetchData();
   }
 
   void getLocation() async{
     MyLocation myLocation = MyLocation();
     await myLocation.getMyCurrentLocation();
+    latitude3 = myLocation.latitude2;
+    longitude3 = myLocation.longitude2;
+    print(latitude3);
+    print(longitude3);
+
+    Network network = Network('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
+
+    var weatherData = await network.getJsonData();
+    print(weatherData);
   }
 
-  void fetchData() async {
-    try {
-      var url = Uri.parse(
-          'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
-      http.Response response = await http.get(url);
-      if (response.statusCode == 200){
-        String jsonData = response.body;
-        var myJson = jsonDecode(jsonData)['weather'][0]['description'];
-        print(myJson);
-        myJson = jsonDecode(jsonData)['wind']['speed'];
-        print(myJson);
-        myJson = jsonDecode(jsonData)['id'];
-        print(myJson);
-      }else{
-        print(response.statusCode);
-      }
-    }catch(e){
-      print('인터넷 파싱에 실패하였습니다');
-    }
-  }
+  // void fetchData() async {
+  //   try {
+  //
+  //       var myJson = parsingData['weather'][0]['description'];
+  //       print(myJson);
+  //       myJson = parsingData['wind']['speed'];
+  //       print(myJson);
+  //       myJson = parsingData['id'];
+  //       print(myJson);
+  //     }else{
+  //       print(response.statusCode);
+  //     }
+  //   }catch(e){
+  //     print('인터넷 파싱에 실패하였습니다');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
