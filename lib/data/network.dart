eeff00319c87c2cef6cb357131c64a2eb8820f68
FlutLab.io <>
2022-07-20 18:28:12 +0000

    Autosave

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -9,7 +9,10 @@ class Network {
     print('-- getJsonData 시작 -- ');
     print(Uri.parse(url));
     http.Response response = await http.get(Uri.parse(url), headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
     });
     if (response.statusCode == 200) {
       String jsonData = response.body;
