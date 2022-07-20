dabf8c5d208ccbb492a214261aa26b223a456256
FlutLab.io <>
2022-07-20 18:28:55 +0000

    Autosave

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -10,7 +10,7 @@ class Network {
     print(Uri.parse(url));
     http.Response response = await http.get(Uri.parse(url), headers: {
       "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Credentials": "true", // Required for cookies, authorization headers with HTTPS
       "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
       "Access-Control-Allow-Methods": "POST, OPTIONS"
     });
