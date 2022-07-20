721a601c94fd18d3d11851b57d3bf2a4caa9119c
FlutLab.io <>
2022-07-20 21:29:33 +0000

    Autosave

--- lib/Screens/weather_screen.dart
+++ lib/Screens/weather_screen.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/cupertino.dart';
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatefulWidget {
-
   WeatherScreen({this.parseWeatherData});
   // 왜 생성자에 컬리브레이스를 하지?
-  // 
+  //
   final parseWeatherData;
-  
+
   @override
   _WeatherScreenState createState() => _WeatherScreenState();
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -24,24 +22,26 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body : SafeArea(
-          child: Center(child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('City Name', 
-              style: TextStyle(
-                fontSize: 30.0,
-              ),),
-              
-              SizedBox(height: 20.0),
-
-              Text('City Name', 
-              style: TextStyle(
-                fontSize: 30.0,
-              ),),
-            ],
-          ),))
-          );
-    );
+        body: SafeArea(
+            child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'City Name',
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            'City Name',
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+        ],
+      ),
+    )));
   }
 }
