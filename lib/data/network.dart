c00d717b8be31e6c99fab7f2ec855ea75544f345
FlutLab.io <>
2022-07-20 17:52:00 +0000

    Autosave

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -7,7 +7,7 @@ class Network {
 
   Future<dynamic> getJsonData() async {
     print('-- getJsonData 시작 -- ');
-    print(Uri.parse(url)));
+    print(Uri.parse(url));
     http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       String jsonData = response.body;
