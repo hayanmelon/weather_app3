67d99e9f43e7b2bf9d1548c3111f1497fd1d6a5a
FlutLab.io <>
2022-07-20 17:14:25 +0000

    Autosave

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -1,16 +1,18 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class Network{
- final String url;
- Network(this.url);
+class Network {
+  final String url;
+  Network(this.url);
 
- Future<dynamic> getJsonData() async {
-  http.Response response = await http.get(Uri.parse(url));
-  if (response.statusCode == 200) {
-   String jsonData = response.body;
-   var parsingData = jsonDecode(jsonData);
-   return parsingData;
+  Future<dynamic> getJsonData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var parsingData = jsonDecode(jsonData);
+      return parsingData;
+    } else {
+      print(response.statusCode);
+    }
   }
- }
 }
