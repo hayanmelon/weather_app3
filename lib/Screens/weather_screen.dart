9ff6457390da34f371a68f10ba9a4b12e25be65f
FlutLab.io <>
2022-07-23 19:00:45 +0000

    Autosave

--- lib/Screens/weather_screen.dart
+++ lib/Screens/weather_screen.dart
@@ -121,7 +121,13 @@ class _WeatherScreenState extends State<WeatherScreen> {
                           Row(
                             children: [
                               SvgPicture.asset('svg/climacon-sun.svg'),
-                              Text('Clear Sky', style: TextStyle(fontStyle: GoogleFonts.lato, fontSize: 16.0, color: Colors.white)),
+                              Text(
+                            'Clear Sky',
+                            style: GoogleFonts.lato(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                            ),
                             ],
                           )
                         ],
