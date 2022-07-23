f623f5fc65a35f7ba147032df10e7a562420f135
FlutLab.io <>
2022-07-23 18:22:32 +0000

    Autosave

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -9,17 +9,22 @@ class Network {
   Future<dynamic> getJsonData() async {
     print('-- getJsonData 시작 -- ');
     print(Uri.parse(url));
-    http.Response response = await http.get(Uri.parse(url), headers: {
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": "true", // Required for cookies, authorization headers with HTTPS
-      "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
-      "Access-Control-Allow-Methods": "POST, OPTIONS"
-    });
-    if (response.statusCode == 200) {
-      String jsonData = response.body;
-      var parsingData = jsonDecode(jsonData);
-      return parsingData;
-    } else {
+
+    try {
+      http.Response response = await http.get(Uri.parse(url), headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": "true", // Required for cookies, authorization headers with HTTPS
+        "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      });
+      if (response.statusCode == 200) {
+        String jsonData = response.body;
+        var parsingData = jsonDecode(jsonData);
+        return parsingData;
+      } else {
+        print(response.statusCode);
+      }
+    } catch (e) {
       var jsonText = await rootBundle.loadString('assets/shop.json');
       var parsingData = jsonDecode(jsonText);
       return parsingData;
