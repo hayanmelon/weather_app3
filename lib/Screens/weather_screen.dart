e506472084a368dbd17ad9fe4c4a9b32648cb4b8
FlutLab.io <>
2022-07-23 18:37:23 +0000

    Autosave

--- lib/Screens/weather_screen.dart
+++ lib/Screens/weather_screen.dart
@@ -4,8 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:timer_builder/timer_builder.dart';
 import 'package:intl/intl.dart';
 
-
-
 class WeatherScreen extends StatefulWidget {
   WeatherScreen({this.parseWeatherData});
   // 왜 생성자에 컬리브레이스를 하지?
@@ -29,7 +27,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
     print(cityName);
   }
 
-  String getSystemTime(){
+  String getSystemTime() {
     var now = DateTime.now();
     return DateFormat("h:mm a").format(now);
   }
@@ -43,127 +41,107 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(extendBodyBehindAppBar: true,
+    return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
         leading: IconButton(
           icon: Icon(Icons.near_me),
-          onPressed: (){},
-
+          onPressed: () {},
         ),
         actions: [
-          IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.location_searching))
+          IconButton(onPressed: () {}, icon: Icon(Icons.location_searching))
         ],
       ),
-      body : Container(
-        child : Stack(
-          children: [
-            Image.asset('image/background.jpg',
-            fit : BoxFit.cover,
-            width : double.infinity,
-              height: double.infinity,
-            ),
-            Container(
-              padding: EdgeInsets.all(50.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column( //구분선 위
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // 지명, 날짜, 시간
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height : 150
-                            ),
-                            Text('Seoul', style: GoogleFonts.lato(
-                              fontSize : 35.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),),
-                            Row(
-                              children: [
-                                TimerBuilder.periodic(
-                                    (Duration(minutes: 1)),
-                                    builder: (context){
-                                      print('${getSystemTime()}');
-                                      return Text(
-                                        '${getSystemTime()}',
-                                        style : GoogleFonts.lato(
-                                          fontSize : 16.0,
-                                          color : Colors.white
-                                        ),
-
-                                      );
-                                    }
-                                ),
-                                Text(
-                                  DateFormat(' - EEEE, ').format(date),
-                                  style : GoogleFonts.lato(
-                                      fontSize : 16.0,
-                                      color : Colors.white
-                                  ),
-                                ),
-                                Text(
-                                  DateFormat('d MMM, yyy').format(date),
-                                  style : GoogleFonts.lato(
-                                      fontSize : 16.0,
-                                      color : Colors.white
-                                  )
-                                ),
-                              ],
-                            )
-
-                          ],
-                        ),
-
-                        // 기온, 기온아이콘콘
-                       Column(
-                          children: [
-                            Text('Seoul', style: GoogleFonts.lato(
-                              fontSize : 35.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-
-                  Column(
+      body: Container(
+          child: Stack(
+        children: [
+          Image.asset(
+            'image/background.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            padding: EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Column(
+                    //구분선 위
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Divider(
-                        height: 15.0,
-                        thickness: 2.0,
-                        color : Colors.white,
-                      ),
-
-                      Row(
+                      // 지명, 날짜, 시간
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          SizedBox(height: 100),
                           Text(
-                            "미세먼지"
+                            'Seoul',
+                            style: GoogleFonts.lato(
+                              fontSize: 35.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
+                          Row(
+                            children: [
+                              TimerBuilder.periodic((Duration(minutes: 1)), builder: (context) {
+                                print('${getSystemTime()}');
+                                return Text(
+                                  '${getSystemTime()}',
+                                  style: GoogleFonts.lato(fontSize: 16.0, color: Colors.white),
+                                );
+                              }),
+                              Text(
+                                DateFormat(' - EEEE, ').format(date),
+                                style: GoogleFonts.lato(fontSize: 16.0, color: Colors.white),
+                              ),
+                              Text(DateFormat('d MMM, yyy').format(date), style: GoogleFonts.lato(fontSize: 16.0, color: Colors.white)),
+                            ],
+                          )
                         ],
                       ),
+
+                      // 기온, 기온아이콘콘
+                      Column(
+                        children: [
+                          Text(
+                            'Seoul',
+                            style: GoogleFonts.lato(
+                              fontSize: 35.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )
                     ],
-                  ), // 구분선 아래
-                ],
-              ),
-            )
+                  ),
+                ),
 
-
-
-          ],
-        )
-      ),
+                Column(
+                  children: [
+                    Divider(
+                      height: 15.0,
+                      thickness: 2.0,
+                      color: Colors.white,
+                    ),
+                    Row(
+                      children: [
+                        Text("미세먼지"),
+                      ],
+                    ),
+                  ],
+                ), // 구분선 아래
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
